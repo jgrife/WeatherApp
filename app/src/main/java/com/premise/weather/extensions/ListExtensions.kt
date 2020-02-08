b8d868074e7155b235670a7f29485f0e565895b0
Jason Grife (PSP) <jgrife@starbucks.com>
2020-02-07 18:13:25 -0800

    Fixing a crash that would happen, if you entered an invalid city such as `zzz`.The `Location` list comes back empty, so we need a `firstOrNull` call and not a `first` call.

--- app/src/main/java/com/premise/weather/extensions/ListExtensions.kt
+++ app/src/main/java/com/premise/weather/extensions/ListExtensions.kt
@@ -7,6 +7,6 @@ import com.premise.weather.models.Location
  * that means that there was a lat/long search and so we return the first results as the feed returns back a list of cities ordered by
  * distance from the lat/long passed in
  */
-fun List<Location>?.getExactMatch(): Location? = if (this?.size == 1 || this?.first()?.distance != null) this.first() else null
+fun List<Location>?.getExactMatch(): Location? = if (this?.size == 1 || this?.firstOrNull()?.distance != null) this.first() else null
 
 fun List<Location>?.isExactMatch(): Boolean = getExactMatch() != null
